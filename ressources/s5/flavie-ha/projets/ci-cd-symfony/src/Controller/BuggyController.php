febce382deb843dcc9f92941c33ce9a12df5a3bc
flvharre <harreflavie@gmail.com>
2025-09-15 21:38:57 +0200

    chore: trigger CI

---
+++ ressources/s5/flavie-ha/projets/ci-cd-symfony/src/Controller/BuggyController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BuggyController extends AbstractController
+{
+    /**
+     * @return array<string, int|float>
+     */
+    private function getData(): array
+    {
+        return [
+            'key1' => 1,
+            'key2' => 2.5,
+        ];
+    }
+
+    #[Route('/bug', name: 'buggy_route')]
+    public function buggyAction(): Response
+    {
+        $data = $this->getData();
+
+        return $this->render('welcome.html.twig', [
+            'data' => $data['key1'],
+        ]);
+    }
+}
